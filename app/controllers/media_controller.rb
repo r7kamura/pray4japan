c233c148c5437b9a763ac637bf3519cff5804393
Ryo NAKAMURA <r7kamura@gmail.com>
2011-03-13 01:15:22 +0900

    show some image from instagram

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,4 +1,16 @@
 class MediaController < ApplicationController
+  before_filter :auth, :only => :index
+
   def index
+    @medias = Instagram.tag_recent_media("playforjapan")
+  end
+
+  private
+  # Instagram Authentication
+  def auth
+    Instagram.configure do |config|
+      config.client_id    = Settings.instagram.client_id
+      config.access_token = Settings.instagram.access_token
+    end
   end
 end
