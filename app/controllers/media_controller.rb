608597c3bffdb3eb5de5ed8d36e1be9ea1f7cd3e
Ryo NAKAMURA <r7kamura@gmail.com>
2011-03-13 07:37:33 +0900

    create ajaxPager

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -2,11 +2,17 @@ class MediaController < ApplicationController
   before_filter :auth, :only => :index
 
   def index
-    @medias = Instagram.tag_recent_media("prayforjapan")
+    @images = Instagram.tag_recent_media("prayforjapan")
+  end
+
+  def more
+    @images = Instagram.tag_recent_media("prayforjapan")
+    respond_to do |format|
+      format.js { render :partial => "images", :layout => false }
+    end
   end
 
   private
-  # Instagram Authentication
   def auth
     Instagram.configure do |config|
       config.client_id    = Settings.instagram.client_id
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,14 @@
+var bindRemote = function(selector) {
+  $(selector)
+    .bind("ajax:success", function(evt, data, status, xhr){
+      $(this).replaceWith(data);
+      bindRemote(selector)
+    });  
+};
+
 $(function(){
   $('.socialButtons .hatena').socialbutton('hatena');
   $('.socialButtons .twitter').socialbutton('twitter', { button: 'horizontal', lang: 'en', related: 'ruedap' });
   $('.socialButtons .facebook').socialbutton('facebook_like', { button: 'button_count', locale: 'en_US' });
+  bindRemote('.moreButton');
 });
