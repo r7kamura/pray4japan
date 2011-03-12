5ccf0ae43c877b3686bfed3c922352fa04db4672
Ryo NAKAMURA <r7kamura@gmail.com>
2011-03-13 01:38:44 +0900

    design using sass

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Pray4japan::Application.initialize!
+
+# Specify sass style
+Sass::Plugin.options[:style] = :compressed 
