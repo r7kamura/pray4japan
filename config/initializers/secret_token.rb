0ff656ad7c599a015ac1a33cb74436df80d9ca08
Ryo NAKAMURA <r7kamura@gmail.com>
2011-03-12 23:31:29 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pray4japan::Application.config.secret_token = '88dfa34db01fcd04403a3242f76f34d2c85f300b3385cc67ebf7fd12a0953a51d84a6f45360724815c4e9f7265d3873f866164f98fc2a9da7ffaca1c7a7d24cc'
