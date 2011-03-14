8325d7cc7281cf113f558647cfd515444df47a55
Ryo NAKAMURA <r7kamura@gmail.com>
2011-03-14 19:18:45 +0900

    add googleAnalytics

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -59,6 +59,18 @@ var setTwitterGadget = function() {
   }).render().start();
 };
 
+var googleAnalytics = function() {
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-22042269-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+};
+
 $(function(){
   $('.socialButtons .hatena').socialbutton('hatena');
   $('.socialButtons .twitter').socialbutton('twitter', { button: 'horizontal', lang: 'en', related: 'ruedap' });
