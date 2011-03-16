ec188a40425d6a1a4e44bc16fde0507234442486
Ryo NAKAMURA <r7kamura@gmail.com>
2011-03-17 01:39:59 +0900

    refactor controller

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -27,14 +27,19 @@ class MediaController < ApplicationController
   private
   def tag_recent_media(tag, *args)
     require "open-uri"
-    uri = "https://api.instagram.com/v1/tags/#{tag}/media/recent?"
-    uri += "access_token=#{Settings.instagram.access_token}"
-    options = args.last.is_a?(Hash) ? args.pop : {}
-    options.each do |k, v|
-      uri += "&#{k}=#{v}"
-    end
-    str = open(uri){|data|data.read}
+    uri = tag_api_uri(tag, Settings.instagram.access_token)
+    uri += to_uri_param(args.pop) if args.last.is_a?(Hash)
+    str = open(uri){|data| data.read}
     json = JSON.parse(str)
     Hashie::Mash.new(json)
   end
+  
+  def tag_api_uri(tag, access_token)
+    "https://api.instagram.com/v1/tags/#{tag}/media/recent?access_token=#{access_token}"
+  end
+
+  def to_uri_param(hash)
+    hash.map{|k,v| "&#{k}=#{v}"}.join
+  end
+
 end
